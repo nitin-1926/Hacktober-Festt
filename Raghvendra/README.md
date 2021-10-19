90a8b6a78c63b2e14fba3d589950d60988bf137d
Raghvendra Singh <raghvendrrsingh@gmail.com>
2021-10-19 13:53:59 +0530

    Added Toast notification

---
+++ Raghvendra/README.md
@@ -0,0 +1,7 @@
+## Toast Notification
+
+Built using HTML, CSS, and JavaScript
+
+## Screenshot 🖼
+
+![Toast Notification Preview](https://user-images.githubusercontent.com/67682451/137870294-0ab3b475-91b4-432c-b9d1-c0b8c1d0b58a.png)
---
+++ Raghvendra/script.js
@@ -0,0 +1,33 @@
+const button = document.getElementById('button')
+const toasts = document.getElementById('toasts')
+
+const messages = [
+    'Designed by Raghvendra Singh',
+    'Welcome to Hacktoberfest 2021!',
+]
+
+const types = ['info', 'success']
+
+button.addEventListener('click', () => createNotification())
+
+function createNotification(message = null, type = null) {
+    const notif = document.createElement('div')
+    notif.classList.add('toast')
+    notif.classList.add(type ? type : getRandomType())
+
+    notif.innerText = message ? message : getRandomMessage()
+
+    toasts.appendChild(notif)
+
+    setTimeout(() => {
+        notif.remove()
+    }, 3000)
+}
+
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)]
+}
+
+function getRandomType() {
+    return types[Math.floor(Math.random() * types.length)]
+}
